e9b4611b27bb93ba5eccbaab1176a05cb77373b5
Aniket3111 <aniketrajani03@gmail.com>
2022-05-11 20:51:42 +0530

    intial

---
+++ kruskals_minimum.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+class Graph:
+    def __init__(self,vertices):
+        self.V=vertices
+        self.graph=[]
+
+    def add_edge(self,u,v,w):
+        self.graph.append([u,v,w])
+    def find(self,parent,i):
+        if parent[i] == i:
+            return i
+        return self.find(parent,parent[i])
+    def union(self,parent,rank,x,y):
+        xroot=self.find(parent,x)
+        yroot=self.find(parent,y)
+
+        if rank[xroot]<rank[yroot]:
+            parent[xroot]=yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[xroot]=yroot
+            rank[xroot]+=1
+    def kruskalmst(self):
+        result=[]
+        i=0
+        e=0
+        self.graph = sorted(self.graph,key=lambda item:item[2])
+        parent=[]
+        rank=[]
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e<self.V-1:
+            u,v,w=self.graph[i]
+            i=i+1
+            x=self.find(parent,u)
+            y=self.find(parent,v)
+            if x!=y:
+                e=e+1
+                result.append([u,v,w])
+                self.union(parent,rank,x,y)
+        minimum_cost=0
+        print("Edges in mst")
+        for u,v,weight in result:
+            minimum_cost+=weight
+            print("%d--%d==%d"%(u,v,weight))
+        print("minimum cost",minimum_cost)
+g=Graph(4)
+g.add_edge(0,1,10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3,5)
+g.add_edge(0,3,6)
+g.add_edge(1,3,15)
+g.add_edge(2,3,4)
+g.kruskalmst()
